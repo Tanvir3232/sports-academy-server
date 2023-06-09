e84f3b8784927d155f48076c220b1857e20bf409
Tanvir Ahmad <tanvirahmadstudent@gmail.com>
2023-06-09 15:56:24 +0600

    implement delete api for delete api

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const cors    = require('cors');
 const jwt = require('jsonwebtoken');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app     = express();
 const port    = process.env.PORT || 5000;
@@ -65,9 +65,16 @@ async function run() {
       next();
     }
     //Users related API
-    app.get('/users',verifyJWT,async(req,res)=>{
+    app.get('/users',verifyJWT,verifyAdmin,async(req,res)=>{
        const result = await userCollection.find().toArray();
        res.send(result);
+    })
+    app.delete('/users/:id',verifyJWT,verifyAdmin,async(req,res)=>{
+      const id = req.params.id;
+      console.log(id);
+      const query = {_id: new ObjectId(id)};
+      const result = await userCollection.deleteOne(query);
+      res.send(result);
     })
     app.get('/users/admin/:email',verifyJWT,async(req,res)=>{
       const email  = req.params.email;
